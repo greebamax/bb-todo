5b588d7edab1e8ff08555bd18667b785526e1911
Max Grib <max.grib@gmail.com>
2017-05-14 01:12:17 +0300

    configure project with rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,15 @@
 import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel'
 
 export default {
   entry: 'src/scripts/main.js',
   dest: 'build/js/bundle.js',
   sourceMap: true,
+  format: 'es',
+  external: [ 'backbone' ],
   plugins: [
-    resolve(),
+    babel({
+      exclude: 'node_modules/**'
+    })
   ]
 };
