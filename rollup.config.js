07a1debb230f1cd5d2584820db26cb18123b137a
Max Grib <max.grib@gmail.com>
2017-05-14 01:57:50 +0300

    finish setup project

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,28 @@
-import resolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel'
+import babel from 'rollup-plugin-babel';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   entry: 'src/scripts/main.js',
   dest: 'build/js/bundle.js',
   sourceMap: true,
-  format: 'es',
-  external: [ 'backbone' ],
+  format: 'iife',
+  external: [ 'jquery' ],
+  globals: {
+    backbone: 'Backbone',
+    underscore: '_',
+    jquery: '$'
+  },
   plugins: [
+    nodeResolve({
+      jsnext: true,
+      main: true,
+      browser: true
+    }),
+    commonjs({
+      include: 'node_modules/**',
+      sourceMap: true
+    }),
     babel({
       exclude: 'node_modules/**'
     })
