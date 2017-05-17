575780cfadd9a4d1a279e19def780a05bdb632d5
Max Grib <max.grib@gmail.com>
2017-05-17 23:47:58 +0300

    install uglify

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import babel from 'rollup-plugin-babel';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import uglify from 'rollup-plugin-uglify';
 
 export default {
   entry: 'src/scripts/main.js',
@@ -20,5 +21,6 @@ export default {
     babel({
       exclude: 'node_modules/**',
     }),
+    uglify(),
   ],
 };
