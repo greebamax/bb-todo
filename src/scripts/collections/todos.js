e03dce34be9820c6d46ccc786e091b787d74deb7
Max Grib <max.grib@gmail.com>
2017-06-14 09:56:49 +0300

    extend todos with completed and remaining methods

--- src/scripts/collections/todos.js
+++ src/scripts/collections/todos.js
@@ -4,4 +4,13 @@ import TodoModel from '../models/todo';
 export default Backbone.Collection.extend({
   url: '/todos',
   model: TodoModel,
+
+  completed() {
+    return this.filter(todo => todo.get('completed'));
+  },
+
+  remaining() {
+    return this.without.apply(this, this.completed());
+  },
+
 });
