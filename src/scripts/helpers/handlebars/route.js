fbd859ed0667154c33ce863baec27a674f535e2a
Max Grib <max.grib@gmail.com>
2018-07-24 23:32:08 +0300

    add possibility to pass className into route hadlebars helper

--- src/scripts/helpers/handlebars/route.js
+++ src/scripts/helpers/handlebars/route.js
@@ -1,17 +1,23 @@
 export default Handlebars => options => {
-  const { fn, hash: { to } } = options;
+  const {
+    fn,
+    hash: {
+      to,
+      className,
+    },
+  } = options;
   const escapedText = Handlebars.escapeExpression(fn(this));
 
   let url = Handlebars.escapeExpression(to);
   const passedArgs = Object.keys(options.hash);
   if (passedArgs.length > 1) {
-    url = passedArgs
-      .reduce((str, key) => {
-        if (key !== 'to') {
-          return str.replace(new RegExp(`{${key}}`), options.hash[key]);
-        }
-        return str;
-      }, url);
+    url = passedArgs.reduce((str, key) => {
+      if (key !== 'to') {
+        return str.replace(new RegExp(`{${key}}`), options.hash[key]);
+      }
+      return str;
+    }, url);
   }
-  return new Handlebars.SafeString(`<a href="#/${url}">${escapedText}</a>`);
+
+  return new Handlebars.SafeString(`<a href="#/${url}" class="nav-link ${className || ''}">${escapedText}</a>`);
 };
