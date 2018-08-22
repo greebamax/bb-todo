4f3e6a6c0aca14ba5961c977ac003f6a8b78050b
Max Grib <max.grib@gmail.com>
2018-08-22 09:52:21 +0300

    fix issue with partials by adding rootImport

--- gulp-tasks/build-scripts.js
+++ gulp-tasks/build-scripts.js
@@ -6,13 +6,24 @@ const { uglify } = require('rollup-plugin-uglify');
 const replace = require('rollup-plugin-replace');
 const handlebars = require('rollup-plugin-handlebars-plus');
 const alias = require('rollup-plugin-alias');
+const rootImport = require('rollup-plugin-root-import');
 const { resolve } = require('path');
 const { ENV } = require('./helpers');
+
+const partialRoots = [
+  `${process.cwd()}/src/scripts`,
+  `${process.cwd()}/src/scripts/common/partials`,
+];
 
 module.exports = async ({ isProd }) => {
   const bundle = await rollup.rollup({
     input: 'src/scripts/main.js',
     plugins: [
+      rootImport({
+        useEntry: 'prepend',
+        extensions: ['.js', '.hbs', '.json'],
+        root: partialRoots,
+      }),
       alias({
         underscore: resolve('node_modules/lodash/index.js'),
         base: resolve('src/scripts/base'),
@@ -34,6 +45,7 @@ module.exports = async ({ isProd }) => {
         },
         helpers: resolve('src/scripts/helpers/handlebars/index.js'),
         jquery: 'jquery',
+        partialRoot: partialRoots,
       }),
       babel({
         exclude: 'node_modules/**',
