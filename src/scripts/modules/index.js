463de9272ef9f53131dd11239915f80d0e538e02
Max Grib <max.grib@gmail.com>
2019-01-24 01:01:42 +0300

    remove unused

--- src/scripts/modules/index.js
+++ src/scripts/modules/index.js
@@ -1,9 +1,7 @@
 import ListsModule from './task-lists';
 import ProfileModule from './profile';
-// import TaskListModule from './task-list';
 
 export default {
   ListsModule,
   ProfileModule,
-  // TaskListModule,
 };
