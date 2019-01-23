1478e95bc37508e9f5a70ac41b469fc674a089af
Max Grib <max.grib@gmail.com>
2019-01-24 01:02:09 +0300

    add possibility to abort requests

--- src/scripts/base/controller.js
+++ src/scripts/base/controller.js
@@ -3,6 +3,7 @@ import Mn from 'backbone.marionette';
 import Radio from 'backbone.radio';
 
 const getRoute = (target, routes) => _.get(routes, _.isFunction(target) ? target.name : target, null);
+const requests = Symbol('layout');
 
 /**
  * @class BaseController
@@ -16,6 +17,8 @@ export default class BaseController extends Mn.Object {
       this.router = options.router;
       this.routes = _.invert(this.__proto__.constructor.appRoutes); // eslint-disable-line
     }
+
+    this[requests] = [];
   }
 
   /**
@@ -36,5 +39,21 @@ export default class BaseController extends Mn.Object {
 
   show(view) {
     Radio.channel('app').request('show:content', view);
+  }
+
+  registerRequest(request) {
+    this[requests].push(request);
+  }
+
+  abortRequests() {
+    for (let i = 0; i < this[requests].length; i++) {
+      const request = this[requests][i];
+
+      if (request !== undefined && request.readyState > 0 && request.readyState < 4) {
+        request.abort();
+      } else if (request.readyState === 4) {
+        this[requests].splice(i, 1);
+      }
+    }
   }
 }
