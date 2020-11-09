47192c9686f1dc383cd8c8c5442e802afbfe1d93
Max Grib <max.grib@gmail.com>
2020-11-10 01:26:07 +0300

    update header template

--- src/scripts/application/header/layout.js
+++ src/scripts/application/header/layout.js
@@ -8,7 +8,7 @@ import HeaderTemplate from './template.tmpl';
 export default class HeaderLayout extends BaseView {
   constructor() {
     super({
-      className: 'navbar-nav',
+      className: 'header',
       tagName: 'ul',
       template: HeaderTemplate,
     });
