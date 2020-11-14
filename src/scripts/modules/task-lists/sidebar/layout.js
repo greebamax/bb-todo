cc9753796abc4b8f706963760a4d09d54ed11384
Max Grib <max.grib@gmail.com>
2020-11-15 00:26:57 +0300

    close sidebar by clicking outside the element

--- src/scripts/modules/task-lists/sidebar/layout.js
+++ src/scripts/modules/task-lists/sidebar/layout.js
@@ -1,9 +1,14 @@
 import Radio from 'backbone.radio';
 import BaseView from 'base/view';
+import ClickOutsideBehavior from 'common/behaviors/click-outside';
 import SideBarTemplate from './template.tmpl';
 
 const LISTS_REGION = 'lists-region';
 const SIDEBAR_OPENED_CLASSNAME = '--opened';
+const EVENTS = {
+  OPENED: 'sidebar:opened',
+  CLOSED: 'sidebar:closed',
+};
 const globalEvents = Radio.channel('app');
 
 /**
@@ -31,12 +36,28 @@ export default class SideBarLayout extends BaseView {
     this.listenTo(globalEvents, 'sidebar:toggle', this.toggleSidebar);
   }
 
+  behaviors() {
+    return [
+      {
+        behaviorClass: ClickOutsideBehavior,
+        handler: this.handleOutsideClick,
+        startListeningEvent: EVENTS.OPENED,
+        stopListeningEvent: EVENTS.CLOSED,
+      },
+    ];
+  }
+
   toggleSidebar(forceState) {
     if (this.isRendered()) {
-      this.isSidebarRegionOpened = this.$el.toggleClass(
-        SIDEBAR_OPENED_CLASSNAME,
-        forceState,
-      );
+      this.isSidebarRegionOpened = this.$el
+        .toggleClass(SIDEBAR_OPENED_CLASSNAME, forceState)
+        .hasClass(SIDEBAR_OPENED_CLASSNAME);
+
+      this.trigger(this.isSidebarRegionOpened ? EVENTS.OPENED : EVENTS.CLOSED);
     }
+  }
+
+  handleOutsideClick() {
+    this.toggleSidebar(false);
   }
 }
