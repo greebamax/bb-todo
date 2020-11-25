75205782596d69fc31204c7e7c472cbe4c608675
Max Grib <max.grib@gmail.com>
2020-11-25 17:01:19 +0300

    add reversible mixin

---
+++ src/scripts/common/mixin/reversible.js
@@ -0,0 +1,24 @@
+import { clone } from 'lodash';
+
+const INIT_ATTRS = Symbol('init-attrs');
+
+export default {
+  init() {
+    this[INIT_ATTRS] = clone(this.attributes);
+  },
+
+  /**
+   * Reset specific field to initial state
+   * @param {String} field
+   */
+  revert(field) {
+    this.set(field, this[INIT_ATTRS][field], { silent: true });
+  },
+
+  /**
+   * Reset all model attributes initial state
+   */
+  revertAll() {
+    this.set(this[INIT_ATTRS], { silent: true });
+  },
+};
