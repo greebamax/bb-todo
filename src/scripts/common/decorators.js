a412208ddfc55161640fe282793c8137cd6c059b
Max Grib <max.grib@gmail.com>
2021-02-10 13:16:07 +0300

    introduce decorators factory

--- src/scripts/common/decorators.js
+++ src/scripts/common/decorators.js
@@ -1,29 +1,34 @@
+import defaults from "lodash/defaults";
 import isArray from "lodash/isArray";
 
 /* eslint-disable import/prefer-default-export */
 
-export function tagName(value) {
-  return function decorator(target) {
-    if (isArray(target.elements)) {
-      target.elements.push({
-        kind: "method",
-        key: "tagName",
-        placement: "prototype",
-        descriptor: { configurable: true, enumerable: false, value },
-      });
-    }
+function decoratorsFactory({ name, kind, placement, descriptorOptions }) {
+  return function fn(value) {
+    return function decorator(target) {
+      if (isArray(target.elements)) {
+        target.elements.push({
+          kind,
+          key: name,
+          placement,
+          descriptor: defaults(
+            { configurable: true, enumerable: false, value },
+            descriptorOptions
+          ),
+        });
+      }
+    };
   };
 }
 
-export function template(value) {
-  return function decorator(target) {
-    if (isArray(target.elements)) {
-      target.elements.push({
-        kind: "method",
-        key: "template",
-        placement: "prototype",
-        descriptor: { configurable: true, enumerable: false, value },
-      });
-    }
-  };
-}
+export const tagName = decoratorsFactory({
+  kind: "method",
+  name: "tagName",
+  placement: "prototype",
+});
+
+export const template = decoratorsFactory({
+  kind: "method",
+  name: "template",
+  placement: "prototype",
+});
