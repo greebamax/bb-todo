f353ab0bb0565bec0b303fb6646b9d2feafe88f5
Max Grib <max.grib@gmail.com>
2021-02-10 15:39:28 +0300

    add support of fields decorators and childView decorator

--- src/scripts/common/decorators.js
+++ src/scripts/common/decorators.js
@@ -1,41 +1,94 @@
+import assign from "lodash/assign";
 import defaults from "lodash/defaults";
 import isArray from "lodash/isArray";
 
 /* eslint-disable import/prefer-default-export */
 
-function decoratorsFactory({ name, kind, placement, descriptorOptions }) {
+/**
+ * Descriptor Options
+ * @typedef {Object} PropertyDescriptor
+ * @property {boolea} configurable
+ * @property {boolean} enumerable
+ * @property {boolean} writable
+ * @property {function} get
+ * @property {function} set
+ * @property {*} value
+ */
+
+/**
+ * Factory Options
+ * @typedef {Object} FactoryOptions
+ * @property {string} name - name of the function, will be used as a `key` of the target prototype
+ * @property {('method'| 'field')} kind - kind of the decorator
+ * @property {PropertyDescriptor} descriptor
+ */
+
+/**
+ * @param {FactoryOptions} param0
+ * @returns Function
+ */
+function decoratorsFactory({ name, kind, descriptor }) {
   return function fn(value) {
     return function decorator(target) {
       if (isArray(target.elements)) {
-        target.elements.push({
+        const elementDefaults = {
           kind,
           key: name,
-          placement,
-          descriptor: defaults(
-            { configurable: true, enumerable: false, value },
-            descriptorOptions
-          ),
-        });
+          placement: "prototype",
+        };
+        let element;
+
+        // eslint-disable-next-line default-case
+        switch (kind) {
+          case "field":
+            element = assign(elementDefaults, {
+              initializer() {
+                return value;
+              },
+              descriptor: defaults(
+                { configurable: true, enumerable: true, writable: true },
+                descriptor
+              ),
+            });
+            break;
+
+          case "method":
+            element = assign(elementDefaults, {
+              descriptor: defaults(
+                { configurable: true, enumerable: false, value },
+                descriptor
+              ),
+            });
+            break;
+        }
+
+        target.elements.push(element);
       }
     };
   };
 }
 
 export const tagName = decoratorsFactory({
-  kind: "method",
+  kind: "field",
   name: "tagName",
-  placement: "prototype",
 });
 
 export const template = decoratorsFactory({
-  kind: "method",
+  kind: "field",
   name: "template",
-  placement: "prototype",
+});
+
+export const className = decoratorsFactory({
+  kind: "field",
+  name: "className",
 });
 
 export const ui = decoratorsFactory({
-  kind: "method",
+  kind: "field",
   name: "ui",
-  placement: "prototype",
-  descriptorOptions: { configurable: true, enumerable: true, writable: true },
+});
+
+export const childView = decoratorsFactory({
+  kind: "field",
+  name: "childView",
 });
